1e5296ed2045d0cf09be70ac7fd6e75f94255b01
waiwai9930 <121942035+waiwai9930@users.noreply.github.com>
2024-03-31 02:23:19 +0800

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -6,12 +6,11 @@ use urlencoding::encode;
 #[no_mangle]
 pub fn translate(
     text: &str, // 待翻译文本
-    from: &str, // 源语言代码
-    to: &str,   // 目标语言代码
-    detect: &str, // 检测到的语言代码(未转换)
-    needs: HashMap<String, String>, // 插件需要的其他配置信息,由info.json定义
-) -> Result<(String, serde_json::Value), Box<dyn Error>> {
-    // 将待翻译文本返回作为结果的一部分
-    // 返回用Value包裹的String
-    return Ok((text.to_string(), Value::String(text.to_string())));
+    from: &str, // 源语言
+    to: &str,   // 目标语言
+    // (pot会根据info.json 中的 language 字段传入插件需要的语言代码，无需再次转换)
+    detect: &str, // 检测到的语言 (若使用 detect, 需要手动转换)
+    needs: HashMap<String, String>, // 插件需要的其他参数,由info.json定义
+) -> Result<Value, Box<dyn Error>> {
+    Ok(Value::String(text.to_string()))
 }
